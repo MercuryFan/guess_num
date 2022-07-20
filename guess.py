c3d8afa2df56befd2dbb27d3ad47739ae467c72e
Scott Fan <tea79649@hotmail.com>
2022-07-20 22:26:14 +0800

    add print_count_think of teacher

--- guess.py
+++ guess.py
@@ -10,16 +10,16 @@ count = 0
 
 r = random.randint(1, 100)
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜猜看,一個數字')
 	num = int(num) #input會強迫答案變成字串,因此要使用型態轉換
 	if num == r:
-		count = count + 1
 		print('終於猜對了,您一共猜了', i, '次')
 		break
 	elif num > r:
-		count = count + 1
 		print('比答案大,在往小點猜')
 	elif num < r:
-		count = count + 1
 		print('比答案小,在往大點猜')
+	print('這是你猜的第', count, '次')
+	# 減少重複性，避免程式延遲
 
