f8d7ceaf771ecafbe00f09e5daa5820625241ffa
Scott Fan <tea79649@hotmail.com>
2022-07-20 22:23:10 +0800

    add print_count_think of me

--- guess.py
+++ guess.py
@@ -3,17 +3,23 @@
 #猜對的話 印出"終於猜對了!"
 #猜錯的話 要告訴他 比答案大或小
 
+#延伸: 印出總共猜幾次
+
 import random
+count = 0
 
 r = random.randint(1, 100)
 while True:
 	num = input('請猜猜看,一個數字')
 	num = int(num) #input會強迫答案變成字串,因此要使用型態轉換
 	if num == r:
-		print('終於猜對了')
+		count = count + 1
+		print('終於猜對了,您一共猜了', i, '次')
 		break
 	elif num > r:
+		count = count + 1
 		print('比答案大,在往小點猜')
 	elif num < r:
+		count = count + 1
 		print('比答案小,在往大點猜')
 
